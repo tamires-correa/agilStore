c4cdaae15648400aa927650c5c50f00a9df91a12
Tamires Correa <tamirescorrea.dev@gmail.com>
2026-01-06 15:10:15 -0300

    adiciona mais opções no menu

--- index.js
+++ index.js
@@ -2,24 +2,29 @@ const readline = require("readline");
 
 const readlineTwo = readline.createInterface({input: process.stdin, output: process.stdout});
 
-function iniciarApp(){
-    console.log("\n=== AgilStore ===");
-    console.log("1 - Adicionar produto");
+function menu(){
+    console.log("\nAgilStore");
+    console.log("──────────\n");
+    console.log("1 - Adicionar Produto");
+    console.log("2 - Listar Produtos");
+    console.log("3 - Atualizar Produto");
+    console.log("4 - Excluir Produto");
+    console.log("5 - Buscar Produto")
     console.log("0 - Sair");
 
-    readlineTwo.question("\n Escolha uma opção:", (opcao) => {
-        console.log(`Você escolheu: ${opcao}`);
+    readlineTwo.question("\nQual opção você deseja?", (opcao) => {
+        console.log(`Opção escolhida: ${opcao}`);
         if (opcao === "1"){
             console.log("Adicionar produto");
-            iniciarApp();
+            menu();
         } else if (opcao === "0"){
             console.log("Encerrando aplicação");
             readlineTwo.close();
         } else {
             console.log("Opção inválida")
-            iniciarApp();
+            menu();
         }
     });
 }
 
-iniciarApp()
+menu()
