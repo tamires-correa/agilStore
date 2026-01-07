f2a4b32485eea1847bf048f581871ed3a9132051
Tamires Correa <tamirescorrea.dev@gmail.com>
2026-01-07 16:53:33 -0300

    implementa lógica tabelaProdutos

--- index.js
+++ index.js
@@ -76,13 +76,70 @@ function addProduto(){
 
 // ===== LISTAR PRODUTOS =====
 function listaProduto(){
-    console.log("Listar produtos - Em desenvolvimento");
+    if (produtos.length === 0){
+        console.log("\nNenhum produto cadastrado ainda");
+        menu();
+        return;
+    }
+// =====  OPÇÃO DE FILTRO/ORDENAÇÃO =====
+console.log("\nOPÇÕES DE FILTRO/ORDENAÇÃO");
+console.log("───────────────────────────────\n");
+console.log("1 - Listar todos os produtos");
+console.log("2 - Filtrar por categoria");
+console.log("3 - Ordernar produtos");
+console.log("0 - Voltar ao menu inicial");
+
+readlineTwo.question("\nEscolha uma opção: ", (opcaoFiltragem) => {
+    switch(opcaoFiltragem){
+        case "1":
+            tabelaProdutos(produtos);
+            break;
+        case "2":
+            filtroCategoria();
+            break;
+        case "3":
+            ordenar();
+            break;
+        case "0":
+            menu();
+            break;
+        default:
+            console.log("\nOpção inválida!");
+            listaProduto();
+        }
+    });
+}
+// =====  TABELA DE PRODUTOS =====
+function tabelaProdutos(lista){
+    console.log("\nPRODUTOS");
+    console.log("──────────\n");
+    console.log("ID:   | Nome:                                | Categoria:                        | Estoque:     | Preço:");
+    console.log("-".repeat(80));
+
+    lista.forEach(pa =>{
+        console.log(`${String(pa.id).padEnd(3)} | ${pa.nome.padEnd(30)} | ${pa.categoria.padEnd(20)} | ${String(pa.estoque).padEnd(5)} | R$ ${pa.preco.toFixed(2).replace('.', ',')}`);
+    });
+    console.log(`\nTotal: ${lista.length} produto(s)`);
+
+    readlineTwo.question("\nPressione ENTER para voltar a tabela", () => {
+        listaProduto();
+    });
+}
+// ===== FILTRAGEM POR CATEGORIA =====
+function filtroCategoria(){
+  console.log("Ordenar produtos. - Em desenvolvimento");
+    menu();
+}
+
+// ===== MENU ORDENAÇÃO =====
+function ordenar(){
+     console.log("Ordenar produtos. - Em desenvolvimento");
     menu();
 }
 
 // ===== ATUALIZAR PRODUTOS =====
 function atualizaProduto(){
-    console.log("Atualizar produtos. - Em desenvolvimento");
+       console.log("Atualizar produtos. - Em desenvolvimento");
     menu();
 }
 
